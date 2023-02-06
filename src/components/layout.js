e53dedd7348117982bc2974cf4c8fbb307b32e70
jyotiprakash055 <jyotiprakash.panigrahi055@gmail.com>
2023-02-07 01:48:48 +0530

    shop

---
+++ src/components/layout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Footer from './footer'
+import Header from './header'
+
+export default function Layout() {
+  return (
+      <>
+          <Header />
+          <Outlet />
+          <Footer />
+
+      </>
+  )
+}

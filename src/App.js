42678da6316a5b4a030d3a86b0d1fadd0d303cd3
jyotiprakash055 <jyotiprakash.panigrahi055@gmail.com>
2023-02-07 13:12:05 +0530

    1.4min

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import React from 'react';
 import Home from './pages/Home'
 import Layout from './components/layout';
 import About from './pages/About';
-
+import Contact from './pages/Contact'
+import './App.css'
 
 function App() {
   return (
-    <React.Fragment>
+    <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Layout />} />
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
+          <Route path='/' element={<Layout />} >
+               <Route index element={<Home />} />
+              <Route path="about" element={<About />} />
+              <Route path="contact" element={<Contact />} />
+            </Route>
           </Routes>
       </BrowserRouter>
-      </React.Fragment>
+      </>
       )
 }
 
